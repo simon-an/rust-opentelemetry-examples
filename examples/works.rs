30141d3ad5f488a50b1dfe0659145e6e237b9d8d
simon-an <26556185+simon-an@users.noreply.github.com>
2021-05-18 10:39:40 +0200

    add a solution to shutdown gracefully

--- examples/works.rs
+++ examples/works.rs
@@ -1,15 +1,20 @@
 use reqwest::Client;
-use std::io;
+use std::{io};
+use tokio::runtime::Handle;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     let _tracer = opentelemetry_zipkin::new_pipeline()
         .with_service_name("tracing_actix_web_opentelemetry_zipkin")
         .with_http_client(Client::new())
-        .install_batch(opentelemetry::runtime::TokioCurrentThread)
+        .install_batch(opentelemetry::runtime::Tokio)
         .expect("pipeline install error");
 
-    opentelemetry::global::shutdown_tracer_provider();
+   
+    Handle::current().spawn_blocking(move || {
+        opentelemetry::global::shutdown_tracer_provider();
+        println!("finished shutdown_tracer_provider");
+    }).await.unwrap();
 
     Ok(())
 }
