1df832b84971a3fd60320067a925ce558bb461c1
simon-an <26556185+simon-an@users.noreply.github.com>
2021-05-18 10:48:15 +0200

    add thread names

--- examples/works.rs
+++ examples/works.rs
@@ -1,5 +1,5 @@
 use reqwest::Client;
-use std::{io};
+use std::{io, thread};
 use tokio::runtime::Handle;
 
 #[actix_web::main]
@@ -10,11 +10,11 @@ async fn main() -> io::Result<()> {
         .install_batch(opentelemetry::runtime::Tokio)
         .expect("pipeline install error");
 
+    println!("hello from thread {}", thread::current().name().unwrap());
    
     Handle::current().spawn_blocking(move || {
         opentelemetry::global::shutdown_tracer_provider();
-        println!("finished shutdown_tracer_provider");
+        println!("finished shutdown_tracer_provider on thread {}", thread::current().name().unwrap());
     }).await.unwrap();
-
     Ok(())
 }
